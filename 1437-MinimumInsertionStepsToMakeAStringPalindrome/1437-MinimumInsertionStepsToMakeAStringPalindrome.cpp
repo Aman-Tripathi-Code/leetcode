3e8f550882349cbd15d80daeaf5f4377ea53fbcd
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-01-11 13:00:43 +0530

    Create 1437-MinimumInsertionStepsToMakeAStringPalindrome

---
+++ 1437-MinimumInsertionStepsToMakeAStringPalindrome/1437-MinimumInsertionStepsToMakeAStringPalindrome.cpp
@@ -0,0 +1,24 @@
+// Last updated: 1/11/2026, 1:00:26 PM
+class Solution {
+public:
+    int minInsertions(string s) {
+        string q = s;
+        reverse(q.begin(),q.end());
+        int n = s.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n; j++){
+                
+                int take = -1e9;
+                if(s[i-1]==q[j-1]){
+                    take = 1 + dp[i-1][j-1];
+                }
+                
+                int notTake = max(dp[i-1][j],dp[i][j-1]);
+
+                dp[i][j] = max(take,notTake);
+            }
+        }
+       return n-dp[n][n];
+    }
+};
