8d8f11813458b568e8dade33443924e139bbecca
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-01-11 13:02:32 +0530

    Create 235-LowestCommonAncestorOfABinarySearchTree

---
+++ 235-LowestCommonAncestorOfABinarySearchTree/235-LowestCommonAncestorOfABinarySearchTree.cpp
@@ -0,0 +1,26 @@
+// Last updated: 1/11/2026, 1:02:15 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root){
+          return NULL;
+        }
+        if(root->val<p->val&&root->val<q->val){
+            return lowestCommonAncestor(root->right,p,q);
+        }
+        if(root->val>p->val&&root->val>q->val){
+            return lowestCommonAncestor(root->left,p,q);
+        }
+        return root;
+    }
+};
