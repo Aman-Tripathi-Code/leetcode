5e79c16b66eed7cbac66d47dee0996b38a689c0f
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-01-11 13:00:07 +0530

    Create 2169-SimpleBankSystem

---
+++ 2169-SimpleBankSystem/2169-SimpleBankSystem.cpp
@@ -0,0 +1,59 @@
+// Last updated: 1/11/2026, 12:59:49 PM
+class Bank {
+public:
+    vector<long long> balance;
+    int n;
+
+    Bank(vector<long long>& balance) {
+        this->balance = balance;
+        this->n = balance.size();
+    }
+    
+    bool transfer(int account1, int account2, long long money) {
+        if(!withdraw(account1,money)){
+            return false;
+        }
+        if(!deposit(account2, money)){
+            deposit(account1,money);
+            return false;
+        }
+        return true;
+    }
+    
+    bool deposit(int account, long long money) {
+        if(!validateAccount(account)){
+            return false;
+        }
+        balance[account-1] += money;
+        return true;
+    }
+    
+    bool withdraw(int account, long long money) {
+        if(!validateAccount(account)){
+            return false;
+        }
+        if(!validateBalance(account, money)){
+            return false;
+        }
+        balance[account-1] -= money;
+        return true;
+    }
+
+    bool validateAccount(int account){
+        if(account < 1 || account > n) return false;
+        return true;
+    }
+
+    bool validateBalance(int acc, long long money){
+        if(balance[acc-1] >= money) return true;
+        return false;
+    }
+};
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank* obj = new Bank(balance);
+ * bool param_1 = obj->transfer(account1,account2,money);
+ * bool param_2 = obj->deposit(account,money);
+ * bool param_3 = obj->withdraw(account,money);
+ */
