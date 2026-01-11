9168589b41aafa316e42e235cf54cdc5a1a78567
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-01-11 13:04:36 +0530

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,29 @@
+// Last updated: 1/11/2026, 1:04:19 PM
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        unordered_map<char,int> mp;
+        int i = 0, j = 0, maxi = 0;
+    
+        while(j<n){
+            mp[s[j]]++;
+            
+            if(mp.size() == j-i+1){
+                maxi = max(maxi,j-i+1);
+            }
+            else if(mp.size() < j-i+1){
+                while(mp.size() < j-i+1){
+                    mp[s[i]]--;
+                    if(mp[s[i]] == 0){
+                        mp.erase(s[i]);
+                    }
+                    i++;
+                }
+                maxi = max(maxi,j-i+1);
+            }
+            j++;
+        }
+        return maxi;
+    }
+};
