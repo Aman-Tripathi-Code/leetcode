a8bda5f88a8abcd7a9e05a56bc11bbd2c10ed422
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-01-11 13:03:21 +0530

    Create 124-BinaryTreeMaximumPathSum

---
+++ 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.cpp
@@ -0,0 +1,30 @@
+// Last updated: 1/11/2026, 1:03:03 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int solver(TreeNode* root,int &maxi){
+        if(root==NULL){
+            return 0;
+        }
+        int leftSum = max(0,solver(root->left,maxi));
+        int rightSum = max(0,solver(root->right,maxi));
+        maxi = max(maxi,leftSum+rightSum+(root->val));
+        return (root->val)+max(leftSum,rightSum);
+    }
+public:
+    int maxPathSum(TreeNode* root) {
+        int maxi = -1e6;
+        int k = solver(root,maxi);
+        return maxi;
+    }
+};
