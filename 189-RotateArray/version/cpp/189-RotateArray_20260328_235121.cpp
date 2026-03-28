a6cb4bdd2950fd1851b52d0696eadd29ef350668
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-03-28 23:51:24 +0530

    Create 189-RotateArray

---
+++ 189-RotateArray/version/cpp/189-RotateArray_20260328_235121.cpp
@@ -0,0 +1,11 @@
+// Last updated: 3/28/2026, 11:51:21 PM
+1class Solution {
+2public:
+3    void rotate(vector<int>& nums, int k) {
+4        int n = nums.size();
+5        k = k % n;
+6        reverse(nums.begin(),nums.end());
+7        reverse(nums.begin(), nums.begin() + k);
+8        reverse(nums.begin() + k, nums.end());  
+9    }
+10};
