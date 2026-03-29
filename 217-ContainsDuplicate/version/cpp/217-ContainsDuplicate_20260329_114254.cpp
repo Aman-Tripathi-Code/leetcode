395218fce6bee646b97d875338fed83882b91832
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-03-29 11:42:54 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/version/cpp/217-ContainsDuplicate_20260329_114254.cpp
@@ -0,0 +1,17 @@
+// Last updated: 3/29/2026, 11:42:54 AM
+1class Solution {
+2public:
+3    bool containsDuplicate(vector<int>& nums) {
+4        int n = nums.size();
+5        set<int> st;
+6        for(int i = 0; i<n; i++){
+7            auto it = st.find(nums[i]);
+8            if(it != st.end()){
+9                return true;
+10            }else{
+11                st.insert(nums[i]);
+12            }
+13        }
+14        return false;
+15    }
+16};
