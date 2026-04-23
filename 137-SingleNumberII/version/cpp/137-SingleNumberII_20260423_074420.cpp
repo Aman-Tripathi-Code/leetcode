57a5ab489c8d430d5e02e15b1314aa49fd8743f0
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-04-23 07:44:24 +0530

    Create 137-SingleNumberII

---
+++ 137-SingleNumberII/version/cpp/137-SingleNumberII_20260423_074420.cpp
@@ -0,0 +1,17 @@
+// Last updated: 4/23/2026, 7:44:20 AM
+1class Solution {
+2public:
+3    int singleNumber(vector<int>& nums) {
+4        int n = nums.size(), ans = 0;
+5        for(int i = 0; i<32; i++){
+6            long long sum = 0;
+7            for(int j = 0; j<n; j++){
+8                sum += ((nums[j] >> i) & 1);
+9            }
+10            if(sum % 3){
+11                ans += (1 << i);
+12            }
+13        }
+14        return ans;
+15    }
+16};
