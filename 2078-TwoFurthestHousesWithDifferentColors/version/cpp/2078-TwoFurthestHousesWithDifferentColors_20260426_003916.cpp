ac721307b9e5aa58a251f7f80075b06029d8b421
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-04-26 00:39:20 +0530

    Create 2078-TwoFurthestHousesWithDifferentColors

---
+++ 2078-TwoFurthestHousesWithDifferentColors/version/cpp/2078-TwoFurthestHousesWithDifferentColors_20260426_003916.cpp
@@ -0,0 +1,18 @@
+// Last updated: 4/26/2026, 12:39:16 AM
+1class Solution {
+2public:
+3    int maxDistance(vector<int>& colors) {
+4        int n = colors.size();
+5        int maxi = -1;
+6        for(int i = 0; i<n-1; i++){
+7            int diff = 0;
+8            for(int j = i+1; j<n; j++){
+9                if(colors[i] != colors[j]){
+10                    diff = j - i;
+11                }
+12            }
+13            maxi = max(diff, maxi);
+14        }
+15        return maxi;
+16    }
+17};
