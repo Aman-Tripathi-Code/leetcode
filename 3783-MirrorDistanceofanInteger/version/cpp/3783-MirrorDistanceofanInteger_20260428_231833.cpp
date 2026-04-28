610d496ed4028c55bdcfd95ddbf7eafe1907b692
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-04-28 23:18:36 +0530

    Create 3783-MirrorDistanceofanInteger

---
+++ 3783-MirrorDistanceofanInteger/version/cpp/3783-MirrorDistanceofanInteger_20260428_231833.cpp
@@ -0,0 +1,17 @@
+// Last updated: 4/28/2026, 11:18:33 PM
+1class Solution {
+2public:
+3    int reverse(int n){
+4        int rev = 0;
+5        while(n){
+6            int rem = n % 10;
+7            rev = 10 * rev + rem;
+8            n /= 10;
+9        }
+10        return rev;
+11    }
+12    int mirrorDistance(int n) {
+13        int rev = reverse(n);
+14        return abs(n - rev);
+15    }
+16};
