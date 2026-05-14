1e9ccc8e7c5e2be66d8150b148fce2ede76f8cf1
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-05-14 21:05:46 +0530

    Create 1791-FindCenterofStarGraph

---
+++ 1791-FindCenterofStarGraph/version/cpp/1791-FindCenterofStarGraph_20260514_210543.cpp
@@ -0,0 +1,12 @@
+// Last updated: 5/14/2026, 9:05:43 PM
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        if(edges[0][0] == edges[1][0] || 
+           edges[0][0] == edges[1][1]) {
+            return edges[0][0];
+        }
+        
+        return edges[0][1];
+    }
+};
