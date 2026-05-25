e009601cb3e553e55f6f5fbcee021526597e83ec
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-05-25 22:11:55 +0530

    Create 3120-CounttheNumberofSpecialCharactersI

---
+++ 3120-CounttheNumberofSpecialCharactersI/version/cpp/3120-CounttheNumberofSpecialCharactersI_20260525_221154.cpp
@@ -0,0 +1,19 @@
+// Last updated: 5/25/2026, 10:11:54 PM
+1class Solution {
+2public:
+3    int numberOfSpecialChars(string word) {
+4        int small = 0, cap = 0;
+5        for(char &ch: word){
+6            int shift = 0;
+7            if(ch >= 'a' && ch <= 'z'){
+8                shift = 1 << (ch - 'a');
+9                small |= shift;
+10            }
+11            if(ch >= 'A' && ch <= 'Z'){
+12                shift = 1 << (ch - 'A');
+13                cap |= shift;
+14            }
+15        }
+16        return __builtin_popcount(small&cap);
+17    }
+18};
