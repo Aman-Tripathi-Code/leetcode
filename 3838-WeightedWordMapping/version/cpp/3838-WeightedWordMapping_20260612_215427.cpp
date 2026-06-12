6ca3548c8bd6879225fe9134153601fca644eb72
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-06-12 21:54:28 +0530

    Create 3838-WeightedWordMapping

---
+++ 3838-WeightedWordMapping/version/cpp/3838-WeightedWordMapping_20260612_215427.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/12/2026, 9:54:27 PM
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+        ans.reserve(words.size());
+        for (string word : words) {
+            int s = 0;
+            for (char c : word) {
+                s += weights[c - 'a'];
+            }
+            ans += 'z' - s % 26;
+        }
+        return ans;
+    }
+};
