fdd4830f778be52c47195a33b9e3eb48a81ba198
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-06-16 09:37:09 +0530

    Create 3612-ProcessStringwithSpecialOperationsI

---
+++ 3612-ProcessStringwithSpecialOperationsI/version/cpp/3612-ProcessStringwithSpecialOperationsI_20260616_093708.cpp
@@ -0,0 +1,20 @@
+// Last updated: 6/16/2026, 9:37:08 AM
+1class Solution {
+2public:
+3    string processStr(string s) {
+4        string ans;
+5        int n = s.length();
+6        for(int i = 0; i<n; i++){
+7            if(s[i] >= 'a' && s[i] <= 'z'){
+8                ans.push_back(s[i]);
+9            }else if(s[i] == '*'){
+10                if(ans.length() > 0) ans.erase(ans.size()-1,1);
+11            }else if(s[i] == '#'){
+12                ans = ans + ans;
+13            }else{
+14                reverse(ans.begin(),ans.end());
+15            }
+16        }
+17        return ans;
+18    }
+19};
