80c95ffb3d247bb6bab9dec59385c91764d780fe
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-06-24 04:42:00 +0530

    Create 347-TopKFrequentElements

---
+++ 347-TopKFrequentElements/version/java/347-TopKFrequentElements_20260624_044159.java
@@ -0,0 +1,37 @@
+// Last updated: 6/24/2026, 4:41:59 AM
+1public class Pair<K,V>{
+2    K key;
+3    V value;
+4
+5    public Pair(K key, V value){
+6        this.key = key;
+7        this.value = value;
+8    }
+9}
+10class Solution {
+11    public int[] topKFrequent(int[] nums, int k) {
+12        int n = nums.length;
+13        int[] kFrequentElements = new int[k];
+14        Map<Integer,Integer> freqMap = new HashMap<>();
+15        Queue<Pair<Integer,Integer>> pq = new PriorityQueue<>((a,b)->(a.key-b.key));
+16
+17        for(int num: nums){
+18            freqMap.put(num, freqMap.getOrDefault(num,0) + 1);
+19        }
+20
+21        for(Map.Entry<Integer,Integer> entry: freqMap.entrySet()){
+22            pq.offer(new Pair<>(entry.getValue(),entry.getKey()));
+23            if(pq.size() > k){
+24                pq.poll();
+25            }
+26        }
+27        int i = 0;
+28        while(!pq.isEmpty()){
+29            kFrequentElements[i] = pq.poll().value;
+30            i++;
+31        }
+32
+33        return kFrequentElements;
+34
+35    }
+36}
