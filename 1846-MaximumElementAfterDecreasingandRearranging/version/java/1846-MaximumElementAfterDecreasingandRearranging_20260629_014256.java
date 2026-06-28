b185c02069fa2ff2dd2207677ccad7ef9dab5bb6
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-06-29 01:43:00 +0530

    Create 1846-MaximumElementAfterDecreasingandRearranging

---
+++ 1846-MaximumElementAfterDecreasingandRearranging/version/java/1846-MaximumElementAfterDecreasingandRearranging_20260629_014256.java
@@ -0,0 +1,14 @@
+// Last updated: 6/29/2026, 1:42:56 AM
+1class Solution {
+2    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+3        int n = arr.length;
+4        Arrays.sort(arr);
+5        arr[0] = 1;
+6        for(int i = 1; i<n; i++){
+7            if(Math.abs(arr[i]-arr[i-1]) > 1){
+8                arr[i] = arr[i-1] + 1;
+9            }
+10        }
+11        return arr[n-1];
+12    }
+13}
