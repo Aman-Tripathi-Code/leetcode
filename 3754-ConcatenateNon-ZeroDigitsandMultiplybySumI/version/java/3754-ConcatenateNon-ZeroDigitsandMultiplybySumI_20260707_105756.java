3a5b4b1b9fb13654ea49e5d8ee6e7a0d4f931547
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-07-07 10:57:57 +0530

    Create 3754-ConcatenateNon-ZeroDigitsandMultiplybySumI

---
+++ 3754-ConcatenateNon-ZeroDigitsandMultiplybySumI/version/java/3754-ConcatenateNon-ZeroDigitsandMultiplybySumI_20260707_105756.java
@@ -0,0 +1,25 @@
+// Last updated: 7/7/2026, 10:57:56 AM
+1class Solution {
+2    long reverseInt(long n){
+3        long rev = 0;
+4        while(n>0){
+5            rev = 10 * rev + n % 10;
+6            n /= 10;
+7        }
+8        return rev;
+9    }
+10    public long sumAndMultiply(int n) {
+11        long num = 0;
+12        int sum = 0;
+13        while(n>0){
+14            int rem = n % 10;
+15            if(rem > 0){
+16                num = (long)(10*num + rem);
+17                sum += rem;
+18            }
+19            n /= 10;
+20        }
+21        num = reverseInt(num);
+22        return (long)(num * sum);
+23    }
+24}
