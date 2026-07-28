269c241a5d6b14adb68d35601fade2fc3dc4804b
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-07-28 20:34:11 +0530

    Create 3517-SmallestPalindromicRearrangementI

---
+++ 3517-SmallestPalindromicRearrangementI/version/cpp/3517-SmallestPalindromicRearrangementI_20260728_203409.cpp
@@ -0,0 +1,12 @@
+// Last updated: 7/28/2026, 8:34:09 PM
+1class Solution {
+2public:
+3    string smallestPalindrome(string s) {
+4        int n = s.size();
+5        sort(s.begin(),s.begin()+n/2);
+6        for(int i = 0; i<n; i++){
+7            s[n-i-1] = s[i];
+8        }
+9        return s;
+10    }
+11};
