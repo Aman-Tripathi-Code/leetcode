802c40a2f3d9dcc2018adc4336c6ae7524bade33
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-07-28 20:43:38 +0530

    Create 3517-SmallestPalindromicRearrangementI

---
+++ 3517-SmallestPalindromicRearrangementI/version/cpp/3517-SmallestPalindromicRearrangementI_20260728_204336.cpp
@@ -0,0 +1,29 @@
+// Last updated: 7/28/2026, 8:43:36 PM
+1class Solution {
+2public:
+3
+4    void countingSort(string &s,int n){
+5        int hs[26];
+6        memset(hs,0,sizeof(hs));
+7
+8        for(int i = 0; i<n/2; i++){
+9            hs[s[i]-'a']++;
+10        }
+11        int ind = 0;
+12        for(int i = 0; i<26; i++){
+13            while(hs[i]){
+14                s[ind++] = char(i+'a');
+15                hs[i]--;
+16            }
+17        }
+18    }
+19
+20    string smallestPalindrome(string s) {
+21        int n = s.size();
+22        countingSort(s,n);
+23        for(int i = 0; i<n/2; i++){
+24            s[n-i-1] = s[i];
+25        }
+26        return s;
+27    }
+28};
