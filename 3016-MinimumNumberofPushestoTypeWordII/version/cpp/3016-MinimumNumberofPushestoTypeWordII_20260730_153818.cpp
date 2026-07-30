860822967162c5cd8d7a94b0b0fa6aa4d2886fc7
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-07-30 15:38:19 +0530

    Create 3016-MinimumNumberofPushestoTypeWordII

---
+++ 3016-MinimumNumberofPushestoTypeWordII/version/cpp/3016-MinimumNumberofPushestoTypeWordII_20260730_153818.cpp
@@ -0,0 +1,21 @@
+// Last updated: 7/30/2026, 3:38:18 PM
+1class Solution {
+2public:
+3    int minimumPushes(string word) {
+4        int hs[26];
+5        memset(hs,0,sizeof(hs));
+6        for(char ch : word){
+7            hs[ch-'a']++;
+8        }
+9        sort(hs,hs+26,greater<int>());
+10        int res = 0, cnt = 0;
+11        for(int i = 0; i<26; i++){
+12            if(hs[i] == 0) break;
+13            if(i % 8 == 0){
+14                cnt++;
+15            }
+16            res += cnt * hs[i];
+17        }
+18        return res;
+19    }
+20};
