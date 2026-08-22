2912e11de65417d82b6d9618ea38d9169d914c88
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-08-22 07:52:16 +0530

    Create 3069-DistributeElementsIntoTwoArraysI

---
+++ 3069-DistributeElementsIntoTwoArraysI/version/cpp/3069-DistributeElementsIntoTwoArraysI_20260822_075211.cpp
@@ -0,0 +1,21 @@
+// Last updated: 8/22/2026, 7:52:11 AM
+1class Solution {
+2public:
+3    vector<int> resultArray(vector<int>& nums) {
+4        int n = nums.size();
+5        vector<int> a, b;
+6        a.push_back(nums[0]);
+7        b.push_back(nums[1]);
+8        for(int i = 2; i<n; i++){
+9            if(a.back() > b.back()){
+10                a.push_back(nums[i]);
+11            }else{
+12                b.push_back(nums[i]);
+13            }
+14        }
+15        for(int i = 0; i<b.size(); i++){
+16            a.push_back(b[i]);
+17        }
+18        return a;
+19    }
+20};
