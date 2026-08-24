dda90c3f7083873c38acfee0f61571040d888a31
Aman Tripathi <74394453+Aman-Tripathi-Code@users.noreply.github.com>
2026-08-25 02:01:27 +0530

    Create 3718-SmallestMissingMultipleofK

---
+++ 3718-SmallestMissingMultipleofK/version/cpp/3718-SmallestMissingMultipleofK_20260825_020127.cpp
@@ -0,0 +1,21 @@
+// Last updated: 8/25/2026, 2:01:27 AM
+1class Solution {
+2public:
+3    int missingMultiple(vector<int>& nums, int k) {
+4        int n = nums.size();
+5        int hs[101] = {0};
+6
+7        for(int i = 0; i<n; i++){
+8            if(nums[i] % k == 0){
+9                hs[nums[i]] = 1;
+10            }
+11        }
+12
+13        for(int i = k; i<=100; i+=k){
+14            if(hs[i] == 0){
+15                return i;
+16            }
+17        }
+18        return ((100/k)+1) * k;
+19    }
+20};
